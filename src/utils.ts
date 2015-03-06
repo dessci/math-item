db8fdb28523e6bc4568e2bd6bb888fa7d166df51
Jan Marthedal Rasmussen <me@janmr.com>
2015-03-06 13:57:49 +0100

    Custom events

--- src/utils.ts
+++ src/utils.ts
@@ -2,34 +2,40 @@ interface Window {
     Promise: any;
 }
 
+interface EventTarget {
+    _flomath_: {
+        events: {
+            [type: string]: EventListener[];
+        };
+    };
+}
+
 module FlorianMath {
+
+    // local interfaces
 
     interface IThen<T> {
         resolved: (val: T) => void;
         rejected: (reason: any) => void;
     }
 
+    // exported interfaces
+
     export interface List<T> {
         length: number;
         [index: number]: T;
     }
 
-    export function each<T>(list: List<T>, fn: (item?: T, index?: number) => void) {
-        for (var k = 0; k < list.length; k++) {
-            fn(list[k], k);
-        }
-    }
-
     export interface PromiseStatic {
         new <T>(callback: (resolve: (val?: T) => void, reject?: (reason: any) => void) => void): IPromise<T>;
         resolve<T>(val?: T): IPromise<T>;
-        //reject(reason?: any): IPromise<void>;
-        //all(promises: IPromise<any>[]): IPromise<any[]>;
     }
 
     export interface IPromise<T> {
         then(resolved: (val?: T) => void, rejected?: (reason: any) => void): IPromise<any>;
     }
+
+    // local functions
 
     function hasPromise(local) {
         return ('Promise' in local && 'resolve' in local.Promise && 'reject' in local.Promise &&
@@ -41,6 +47,21 @@ module FlorianMath {
                     });
                     return typeof resolve === 'function';
                 }));
+    }
+
+    function indexOf<T>(list: T[], item: T) {
+        for (var k = 0; k < list.length; k++)
+            if (list[k] === item)
+                return k;
+        return -1;
+    }
+
+    // exported functions
+
+    export function each<T>(list: List<T>, fn: (item?: T, index?: number) => void) {
+        for (var k = 0; k < list.length; k++) {
+            fn(list[k], k);
+        }
     }
 
     export var Promise: PromiseStatic = hasPromise(window)
@@ -127,5 +148,100 @@ module FlorianMath {
             var regex = new RegExp('^' + characters + '+|' + characters + '+$', 'g');
             return (st: string) => st.replace(regex, '');
         })();
+
+    // Custom events
+
+    interface EventInitParams {
+        bubbles?: boolean;
+        cancelable?: boolean;
+        detail?: any
+    }
+
+    function hasCustomEvent() {
+        try {
+            return (typeof CustomEvent === 'function') && (new (<any> CustomEvent)('ev'));
+        }
+        catch (ex) {
+            return false;
+        }
+    }
+
+    export var dispatchCustomEvent: (target: EventTarget, typeArg: string, params?: EventInitParams) => void;
+    export var addCustomEventListener: (target: EventTarget, typeArg: string, listener: EventListener) => void;
+    export var removeCustomEventListener: (target: EventTarget, typeArg: string, listener: EventListener) => void;
+
+    if (window.addEventListener) {
+        if (hasCustomEvent()) {
+            dispatchCustomEvent = (target: EventTarget, typeArg: string, params?: EventInitParams) => {
+                var evt = new (<any> CustomEvent)(typeArg, params);
+                target.dispatchEvent(evt);
+            };
+        } else if (document.createEvent) {
+            // IE >= 9
+            dispatchCustomEvent = (target: EventTarget, typeArg: string, params?: EventInitParams) => {
+                var evt = document.createEvent('CustomEvent');
+                params = params || { bubbles: false, cancelable: false, detail: undefined };
+                evt.initCustomEvent(typeArg, params.bubbles, params.cancelable, params.detail);
+                target.dispatchEvent(evt);
+            };
+        }
+        if (dispatchCustomEvent) {
+            addCustomEventListener = (target: EventTarget, typeArg: string, listener: EventListener) => {
+                target.addEventListener(typeArg, listener);
+            };
+            removeCustomEventListener = (target: EventTarget, typeArg: string, listener: EventListener) => {
+                target.removeEventListener(typeArg, listener);
+            };
+        }
+    }
+
+    if (!dispatchCustomEvent) {
+        // IE 8
+        (function () {
+            function CustomEvent(target: EventTarget, typeArg: string, params: EventInitParams) {
+                params = params || { bubbles: false, detail: undefined };
+                this.type = typeArg;
+                this.target = target;
+                this.detail = params.detail;
+                this.bubbles = params.bubbles;
+                this.cancelBubble = false;
+            }
+            CustomEvent.prototype.stopPropagation = function () {
+                this.cancelBubble = true;
+            };
+
+            dispatchCustomEvent = (target: EventTarget, typeArg: string, params?: EventInitParams) => {
+                var evt = new CustomEvent(target, typeArg, params);
+                while (target) {
+                    if (target._flomath_ && target._flomath_.events && typeArg in target._flomath_.events) {
+                        each(target._flomath_.events[typeArg], (fn: EventListener) => {
+                            fn(evt);
+                        });
+                    }
+                    if (!evt.bubbles || evt.cancelBubble || !(<Node> target).parentNode)
+                        break;
+                    target = (<Node> target).parentNode;
+                }
+            };
+
+            addCustomEventListener = (target: EventTarget, typeArg: string, listener: EventListener) => {
+                if (!target._flomath_)
+                    target._flomath_ = { events: {} };
+                var eventDict = target._flomath_.events;
+                if (!(typeArg in eventDict))
+                    eventDict[typeArg] = [];
+                eventDict[typeArg].push(listener);
+            };
+
+            removeCustomEventListener = (target: EventTarget, typeArg: string, listener: EventListener) => {
+                if (target._flomath_ && target._flomath_.events && typeArg in target._flomath_.events) {
+                    var listeners = target._flomath_.events[typeArg],
+                        index = indexOf(listeners, listener);
+                    if (index >= 0)
+                        listeners.splice(index, 1);
+                }
+            };
+        })();
+    }
 
 }
